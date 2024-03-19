898061e698df1840f74172b31aaf039ddefbb556
Gabriel Vieira <gabrielsy.cacamba@gmail.com>
2024-03-19 11:22:35 -0300

    Add files via upload
    Simulador de Dado em Python

---
+++ dado-simulator.py
@@ -0,0 +1,11 @@
+import time, os, random
+print('Bem vindo ao gira-dado ')
+time.sleep(2)
+os.system('cls')
+tamanho = int(input('Qual tamanho do dado você deseja girar? '))
+dado = random.randint(1, tamanho)
+girar = str(input('Deseja girar o dado? (s/n) ')).upper()
+if girar == 'SIM' or girar == 'S':
+    print('D{}'.format(dado))
+elif girar == 'NAO' or girar == 'N':
+    print('Ok! saindo.')
